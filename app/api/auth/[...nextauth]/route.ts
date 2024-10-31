27c40d841772b0e627ddbb308492dcd9b64a3452
Fahad <fahadkhann0204@gmail.com>
2024-10-31 16:03:35 +0530

    typescript error resolved try 8

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,60 +1,6 @@
-// app/api/auth/[...nextauth]/route.ts
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import prisma from '@/lib/prisma';
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth, { AuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import { v4 as uuidv4 } from 'uuid';
-import { NextResponse } from 'next/server';
+import NextAuth from 'next-auth'
+import { authOptions } from '@/app/utils/authOptions'
 
-export const authOptions: AuthOptions = {
-    session: {
-        strategy: 'jwt'
-    },
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-            profile(profile) {
-                const memberID = `BOOKIE/${uuidv4().slice(0, 5).toUpperCase()}`;
-                return {
-                    id: profile.sub,
-                    name: `${profile.given_name} ${profile.family_name}`,
-                    email: profile.email,
-                    image: profile.picture,
-                    memberID,
-                    role: profile.email === "fahad.khan2216@gmail.com" ? "admin" : "user",
-                    subscription: {
-                        create: {
-                            planType: 'BASIC',
-                            status: 'ACTIVE',
-                            startDate: new Date(),
-                            endDate: new Date(new Date().setMonth(new Date().getMonth() + 1)),
-                        },
-                    },
-                };
-            }
-        })
-    ],
-    callbacks: {
-        async jwt({ token, user }) {
-            return { ...token, ...user };
-        },
-        async session({ session, token }) {
-            session.user.role = token.role;
-            return session;
-        }
-    }
-};
+const handler = NextAuth(authOptions);
 
-// Define GET and POST methods
-export async function GET(req: Request) {
-    const response = await NextAuth(req);
-    return response;
-}
-
-export async function POST(req: Request) {
-    const response = await NextAuth(req);
-    return response;
-}
+export { handler as GET, handler as POST };
---
+++ app/utils/authOptions.ts
@@ -0,0 +1,47 @@
+import prisma from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import GoogleProvider from 'next-auth/providers/google'
+import { v4 as uuidv4 } from 'uuid';
+
+export const authOptions: AuthOptions = {
+    session: {
+        strategy: 'jwt'
+    },
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            profile(profile) {
+
+                const memberID = `BOOKIE/${uuidv4().slice(0, 5).toUpperCase()}`;
+                return ({
+                    id: profile.sub,
+                    name: `${profile.given_name} ${profile.family_name}`,
+                    email: profile.email,
+                    image: profile.picture,
+                    memberID,
+                    role: profile.email == "fahad.khan2216@gmail.com" ? "admin" : "user",
+                    subscription: {
+                        create: {
+                        planType: 'BASIC',
+                        status: 'ACTIVE',
+                        startDate: new Date(),
+                        endDate: new Date(new Date().setMonth(new Date().getMonth() + 1)),
+                        },
+                    },
+                })
+            }
+        })
+    ],
+    callbacks: {
+        async jwt({token, user}) {
+            return {...token, ...user};
+        },
+        async session({ session, token }) {
+            session.user.role = token.role;
+            return session;
+        }
+    }
+}
