9786a36e0f7747145449146085abf6bca34854cc
Fahad <fahadkhann0204@gmail.com>
2024-10-31 16:08:50 +0530

    typescript error resolved try 9

--- app/(RBAC)/user/dashboard/page.tsx
+++ app/(RBAC)/user/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { UserDashboardMain } from '@/components/Main-Dashboards/user-dashboard';
 import React from 'react'
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/utils/authOptions';
 
 const page = async () => {
   const session = await getServerSession(authOptions);
