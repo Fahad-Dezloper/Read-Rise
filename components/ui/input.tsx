58800a0c521795f0ef686c00e70dfdec8b7b961e
Fahad <fahadkhann0204@gmail.com>
2024-10-31 15:15:11 +0530

    typescript error resolved try 2

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
