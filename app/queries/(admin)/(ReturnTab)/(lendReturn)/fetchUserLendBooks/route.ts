d215a935a83dfc18a616fa337c1ca06e08660be3
Fahad <fahadkhann0204@gmail.com>
2024-11-01 21:24:03 +0530

    try 43

--- app/queries/(admin)/(ReturnTab)/(lendReturn)/fetchUserLendBooks/route.ts
+++ app/queries/(admin)/(ReturnTab)/(lendReturn)/fetchUserLendBooks/route.ts
@@ -1,4 +1,5 @@
 // app/api/UsersLendBooks/route.ts
+export const dynamic = "force-dynamic";
 import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client';
 
--- app/queries/(admin)/(ReturnTab)/(saleReturn)/fetchUserPurchasedBooks/route.ts
+++ app/queries/(admin)/(ReturnTab)/(saleReturn)/fetchUserPurchasedBooks/route.ts
@@ -1,4 +1,5 @@
 // app/api/UsersPurchasedBooks/route.ts
+export const dynamic = "force-dynamic";
 import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client';
 
